5abc7223e07b32b7195c1af0a3c7d7e9088e8a53
Blake Anneberg <blakeanneberg@blakes-mbp-2.lan>
2019-12-16 21:34:06 -0700

    age stuff

--- Accessing Object Properties/js/object.js
+++ Accessing Object Properties/js/object.js
@@ -1,7 +1,7 @@
 var person = {
   name : 'Blake A',
   country : 'US',
-  age : 35,
+  age : 33,
   treehouseStudent : true,
   skills : ['JavaScript', 'HTML', 'CSS']
 };
@@ -15,7 +15,7 @@ var message = '<p>Hello. My name is ' + person.name + '.</p>';
 message += '<p>I live in the ' + person.country + '.</p>';
 person.name = 'Paco Denver';
 message += '<p>But, I wish my name was ' + person.name + '.</p>';
-person.age += -1;
+person.age += 1;
 message += '<p>My age is ' + person.age + '.</p>';
 message += '<p>I have ' + person.skills.length + ' skills.</p>';
 
