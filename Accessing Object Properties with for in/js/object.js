5ba117f0e2cc2a1f9cb21c91d8194d06ed02ed2d
Blake Anneberg <blakeanneberg@blakes-mbp-2.lan>
2019-12-16 21:59:06 -0700

    added for in section for object properties

---
+++ Accessing Object Properties with for in/js/object.js
@@ -0,0 +1,28 @@
+var person = {
+  name : 'Blake A',
+  country : 'US',
+  age : 33,
+  treehouseStudent : true,
+  skills : ['JavaScript', 'HTML', 'CSS']
+};
+
+let (prop in person){
+  console.log(prop);
+}
+
+
+
+// function print(message) {
+//   var div = document.getElementById('output');
+//   div.innerHTML = message;
+// }
+
+// var message = '<p>Hello. My name is ' + person.name + '.</p>';
+// message += '<p>I live in the ' + person.country + '.</p>';
+// person.name = 'Paco Denver';
+// message += '<p>But, I wish my name was ' + person.name + '.</p>';
+// person.age += 1;
+// message += '<p>My age is ' + person.age + '.</p>';
+// message += '<p>I have ' + person.skills.length + ' skills.</p>';
+// message += person.skills.join(', ') + '</p>';
+// print(message);
