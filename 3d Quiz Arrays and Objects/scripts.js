3aa078614e3fbcad386a080601056abedc92dae3
Blake Anneberg <blakeanneberg@blakes-mbp-2.lan>
2019-12-23 15:09:28 -0700

    created 3d quize and arrays and objects

---
+++ 3d Quiz Arrays and Objects/scripts.js
@@ -0,0 +1,68 @@
+
+var questions = [
+    ['States in USA', 50],
+    ['Contents in world', 7 ],
+    ['Legs humans have', 2 ]
+];
+
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var correct = [];
+var wrong = [];
+
+
+function print (message) {
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = message;
+}
+
+function buildList(arr){
+    var listHTML = '<ol>';
+        for (var i = 0; i < arr.length; i += 1) {
+            listHTML += '<li>' + arr[i] + '</li>';
+        }
+        listHTML += '</ol>';
+        return listHTML;
+}
+
+for ( var i = 0; i < questions.length; i += 1 ){
+    question = questions[i][0];
+    answer = questions[i][1];
+    response = parseInt(prompt(question));
+    if (response === answer){
+        correctAnswers += 1;
+        correct.push(question);
+    } else {
+        wrong.push(question);
+    }
+}
+
+html = "You got " + correct + " question(s) right."
+html += '<h2>You got these questions correct:</h2>';
+html += buildList(correct);
+html += '<h2> You got these questions wrong:</h2>';
+html += buildList(wrong);
+print(html);
+
+/*
+
+function printQuestions  ( questions ) {
+    var listHTML = '<ol>';
+    for ( var i = 0; i < questions.length; i += 1 ) {
+        let sign = prompt(questions[i][0]);
+        if (sign.toLowerCase() == questions[i][1]) {
+            alert("Correct");
+          }
+        listHTML += '<li>' + questions[i][0] + ' - ' + questions[i][1] + '</li>';
+    }
+    listHTML += '</ol>';
+    function print (message) {
+        document.write(message);
+}
+}
+printQuestions(questions);
+
+*/
+
